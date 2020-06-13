0035a2fc8e10df298b037ea0d8ea3cdb5ce4768d
Matheus Castello <matheus@castello.eng.br>
2020-06-13 20:02:06 -0300

    Config the builds for my env
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- TotalCrossVM/builders/gcc-linux-arm/tcvm/build.sh
+++ TotalCrossVM/builders/gcc-linux-arm/tcvm/build.sh
@@ -3,14 +3,17 @@ BASEDIR=$(dirname $0)
 WORKDIR=$(cd $BASEDIR; pwd)
 
 # reset multi-arch
-sudo docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 # execute docker run
-sudo docker run \
--v $WORKDIR:/build \
--v $WORKDIR/../../../deps/skia:/skia \
--v $WORKDIR/../../../src:/src \
--e SRCDIR=/../../../src \
--e LIBS="-L. -lskia -lstdc++ -lpthread -lEGL -lfontconfig -lSDL2main -lSDL2" \
--t totalcross/cross-compile \
-bash -c "make  -j$(($(nproc) + 2)) -f /build/Makefile"
+docker run \
+    --rm \
+    -v $WORKDIR:/build \
+    -v $WORKDIR/../../../deps/skia:/skia \
+    -v $WORKDIR/../../../src:/src \
+    -e SRCDIR=/../../../src \
+    -e LIBS="-L. -lskia -lstdc++ -lpthread -lEGL -lfontconfig -lSDL2main -lSDL2" \
+    -t totalcross/cross-compile \
+    bash -c "make  -j$(($(nproc) + 2)) -f /build/Makefile"
+
+cp bin/libtcvm.so $PATH_TO_VALID_SDK/dist/vm/linux_arm
--- TotalCrossVM/builders/gcc-posix/tcvm/build.sh
+++ TotalCrossVM/builders/gcc-posix/tcvm/build.sh
@@ -5,13 +5,15 @@ WORKDIR=$(cd $BASEDIR; pwd)
 sudo rm -Rf bin
 
 # execute docker run
-sudo docker run \
--v $WORKDIR:/build \
--v $WORKDIR/../../../deps/skia:/skia \
--v $WORKDIR/../../../src:/src \
--e SRCDIR=/../../../src \
--e LIBS="-L. -lskia -lstdc++ -lpthread -lfontconfig -lGL -lSDL2main -lSDL2" \
--t totalcross/amd64-cross-compile:bionic \
-bash -c "make  -j$(($(nproc) + 2)) -f /build/Makefile"
+docker run \
+    --rm \
+    -v $WORKDIR:/build \
+    -v $WORKDIR/../../../deps/skia:/skia \
+    -v $WORKDIR/../../../src:/src \
+    -e SRCDIR=/../../../src \
+    -e LIBS="-L. -lskia -lstdc++ -lpthread -lfontconfig -lGL -lSDL2main -lSDL2" \
+    -t totalcross/amd64-cross-compile:bionic \
+    bash -c "make  -j$(($(nproc) + 2)) -f /build/Makefile"
 
-# bash -c "make  -j$(($(nproc) + 2)) -f ${WORKDIR}/Makefile"
+# copy the artifacts
+cp bin/libtcvm.so $PATH_TO_VALID_SDK/dist/vm/linux
